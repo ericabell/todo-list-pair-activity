d4b00bc0d12b9cef59bd46361847111dfa6d4540
Eric Abell <ericabell@users.noreply.github.com>
2017-08-15 13:37:24 -0500

    Basic server up and running

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const mustacheExpress = require('mustache-express');
 
 const app = express();
 
+let todos = [
+  {name: 'todo1', completed: true},
+  {name: 'todo2', completed: true},
+  {name: 'todo3', completed: true},
+  {name: 'todo4', completed: false},
+  {name: 'todo5', completed: false},
+]
+
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
@@ -11,6 +19,7 @@ app.set('view engine', 'mustache')
 //Listening on root
 app.get('/todo/', function (req, res) {
   // TODO write your code here
+  res.send('Hello world!');
 })
 
 app.listen(3000, function () {
